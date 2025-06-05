8368fa13848ef35cf0b51b9f31b1c7d96d1fe6c9
LrestJ <rest@seznam.cz>
2025-06-05 16:29:31 +0200

    Script add

--- .dotfiles/scripts/installeos.sh
+++ .dotfiles/scripts/installeos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PacmanApps="gvim hyprland gvfs xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk sddm eos-sddm-theme udiskie simple-scan sddm eos-sddm-theme mako rofi rofimoji  ttf-nerd-fonts-symbols-common ttf-hack-nerd awesome-terminal-fonts yazi fish foot waybar hyprpolkitagent hypridle wlsunset filelight figlet swaybg pcmanfm-qt featherpad libreoffice-fresh-cs qt6ct oxygen-icons brightnessctl btop fastfetch kalk partitionmanager krename mpv vlc okular pamixer pdfarranger qjackctl qpwgraph xournalpp zip p7zip wlsunset kwalletmanager loupe"
+PacmanApps="gvim hyprland gvfs xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk sddm eos-sddm-theme udiskie simple-scan sddm eos-sddm-theme mako rofi rofimoji  ttf-nerd-fonts-symbols-common ttf-hack-nerd awesome-terminal-fonts yazi fish foot waybar hyprpolkitagent hypridle wlsunset filelight figlet swaybg pcmanfm-qt featherpad libreoffice-fresh-cs qt6ct oxygen-icons brightnessctl btop fastfetch kalk partitionmanager krename mpv vlc okular pamixer pdfarranger qjackctl qpwgraph xournalpp zip p7zip wlsunset kwalletmanager kwallet-pam loupe"
 
 AurApps="clipse waypaper brave-bin wlogout hyprshot hyprlock figlet-fonts hypridle"
 
@@ -13,12 +13,12 @@ echo "Instalace dokončena"
 sudo systemctl enable sddm.service
 sleep 3
 echo "Kopíruji konfiguraci z repotitáře"
-alias cfg='git --git-dir=$HOME/.cfg-git/ --work-tree=$HOME'
+#alias cfg='git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME' &&
 echo ".cfg.git" >> .gitignore
-git clone --bare https://codeberg.org/lrestj/endos.git $HOME/.cfg.git
-alias cfg='git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME'
-cfg checkout -f
-cfg config --local status.showUntrackedFiles no
+git clone --bare https://codeberg.org/lrestj/endos.git $HOME/.cfg.git &&
+#alias cfg='git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME' &&
+git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME checkout -f
+git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME config --local status.showUntrackedFiles no
 echo "Konfigurace z repozitáře kompletní"
 
 # git@github.com:lrestj/endos.git
