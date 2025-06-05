7ebbfa48ed6c9c8d812c246f7cb1553543d1b419
LrestJ <rest@seznam.cz>
2025-06-05 14:26:35 +0200

    Scripts added

---
+++ .dotfiles/scripts/2nfs mounts.sh
@@ -0,0 +1,50 @@
+#KLIENT NASTAVENÍ:
+#!/bin/bash
+
+#sudo apt install nfs-common
+
+sudo systemctl enable --now nfs-client.target
+
+sudo systemctl enable --now NetworkManager-wait-online.service
+
+
+#Ověřit:
+
+showmount -e 192.168.77.18
+
+
+
+#mounts
+
+
+sudo mkdir -p /data/nfs/FilmyNas /data/nfs/HudbaNas /data/nfs/Nas &&
+
+sudo chmod -R ugo+rwx /data/nfs
+
+sudo mount -t nfs 192.168.77.18:/volume1/Rodinas /data/nfs/Nas
+
+sudo mount -t nfs 192.168.77.18:/volume1/Hudba /data/nfs/HudbaNas
+
+sudo mount -t nfs 192.168.77.18:/volume1/Filmy /data/nfs/FilmyNas
+
+#add to fstab:
+sudo cp /etc/fstab /etc/fstab.bak
+
+sudo cat 03fstabnfs | tee -a /etc/fstab
+
+echo "Připojení Nas Synology proběhlo úspěšně"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
