10c46ba120432d72f814c144b1c79bdd78568b52
LrestJ <rest@seznam.cz>
2025-06-05 18:30:24 +0200

    Scripting succesful

--- .dotfiles/scripts/2nfs mounts.sh
+++ .dotfiles/scripts/2nfs mounts.sh
@@ -30,21 +30,18 @@ sudo mount -t nfs 192.168.77.18:/volume1/Filmy /data/nfs/FilmyNas
 #add to fstab:
 sudo cp /etc/fstab /etc/fstab.bak
 
+cd ~/.dotfiles/scripts/
 cat 3fstabnfs | sudo tee -a /etc/fstab
+echo -e "\n"
 
 echo "Připojení Nas Synology proběhlo úspěšně"
+echo "Soubor fstab nyní vypadá takto:"
+echo "-------------------------------------------------------------------------"
+echo -e "\n"
+cat /etc/fstab
+
+echo -e "\n"
+echo "-------------------------------------------------------------------------"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+##### END OF FILE #####
