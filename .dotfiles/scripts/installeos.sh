0364e13ccf87af11fcabcb7135d826bbc6fc8a12
LrestJ <rest@seznam.cz>
2025-06-07 19:37:48 +0200

    Qalculate-qt added to install script

--- .dotfiles/scripts/installeos.sh
+++ .dotfiles/scripts/installeos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PacmanApps="neovim hyprland gvfs xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk sddm eos-sddm-theme udiskie simple-scan sddm eos-sddm-theme breeze mako rofi rofimoji  ttf-nerd-fonts-symbols-common ttf-hack-nerd awesome-terminal-fonts yazi fish foot waybar hyprpolkitagent hypridle wlsunset filelight figlet swaybg pcmanfm-qt featherpad libreoffice-fresh-cs qt6ct oxygen-icons brightnessctl btop fastfetch kalk partitionmanager krename mpv vlc okular pamixer pdfarranger qjackctl qpwgraph xournalpp zip p7zip wlsunset kwalletmanager kwallet-pam loupe"
+PacmanApps="neovim hyprland gvfs xdg-desktop-portal xdg-desktop-portal-hyprland xdg-desktop-portal-gtk sddm eos-sddm-theme udiskie simple-scan sddm eos-sddm-theme breeze mako rofi rofimoji  ttf-nerd-fonts-symbols-common ttf-hack-nerd awesome-terminal-fonts yazi fish foot waybar hyprpolkitagent hypridle wlsunset filelight figlet swaybg pcmanfm-qt featherpad libreoffice-fresh-cs qt6ct oxygen-icons brightnessctl btop fastfetch qalculate-qt partitionmanager krename mpv vlc okular pamixer pdfarranger qjackctl qpwgraph xournalpp zip p7zip wlsunset kwalletmanager kwallet-pam loupe"
 
 AurApps="clipse waypaper brave-bin wlogout hyprshot hyprlock figlet-fonts hypridle"
 
