3dbfb73412852ef1a1e79092e33692dffbe81db2
LrestJ <rest@seznam.cz>
2026-04-13 14:00:48 +0200

    Fix

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,9 +16,13 @@ abbr rem "sudo pacman -Rns"
 
 abbr cfg "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME"
 abbr ga "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME add"
+abbr nga "cd $HOME/.dotfiles/install && git add"
 abbr gc --set-cursor -- "git --git-dir $HOME/.cfg.git/ --work-tree=$HOME commit -a -m \"%\""
+abbr ngc --set-cursor -- "git commit -a -m \"%\""
 abbr ppp "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME push -u github endeavourOS && git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME push gitlab endeavourOS"
+abbr npp "cd $HOME/.dotfiles/install && git push -u github main && git push gitlab main"
 abbr gs "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME status"
+abbr ngs "cd $HOME/.dotfiles/install && git status"
 abbr str "git --git-dir=/home/libor/.cfg.git/ --work-tree=/home/libor log --pretty=format: --name-only --diff-filter=A "
 abbr untrack "git --git-dir=$HOME/.cfg.git/ --work-tree=$HOME update-index --skip-worktree"
 
