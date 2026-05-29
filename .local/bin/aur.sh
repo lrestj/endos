0196643949c93aa29c4318afc89afe984ec1d180
LrestJ <rest@seznam.cz>
2026-05-29 08:21:37 +0000

    Fixes

--- .local/bin/aur.sh
+++ .local/bin/aur.sh
@@ -13,10 +13,6 @@ fi
 PKG_NAME="$1"
 BUILD_DIR="$HOME/.local/src"
 
-# Krok 1: Instalace potřebných závislostí pro sestavení
-echo "==> Kontrola a instalace základních nástrojů (base-devel, git)..."
-sudo pacman -S --needed --noconfirm base-devel git
-
 # Krok 2: Vytvoření čistého adresáře pro sestavení
 mkdir -p "$BUILD_DIR"
 cd "$BUILD_DIR"
@@ -25,14 +21,31 @@ cd "$BUILD_DIR"
 if [ -d "$PKG_NAME" ]; then
     echo "==> Adresář $PKG_NAME již existuje. Aktualizuji repozitář..."
     cd "$PKG_NAME"
-    git pull
+    git fetch origin
+    LOCAL_HASH=$(git rev-parse HEAD)
+    REMOTE_HASH=$(git rev-parse @{u}) # @{u} is shorthand for the upstream branch
+    if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+        echo "##########################################"
+        echo "#  Aktualizace k dispozici, provádím...  #"
+        echo "##########################################"
+        sleep 3
+        git pull
+        makepkg -sir
+        echo "################################"
+        echo "#  Aktualizace kompletní.      #"
+        echo "################################"
+    else
+        echo "#####################################"
+        echo "#  Žádné dostupné aktualizace.      #"
+        echo "#####################################"
+        exit 0
+    fi
 else
     echo "==> Klonování repozitáře $PKG_NAME z AUR..."
 git clone "https://aur.archlinux.org/${PKG_NAME}.git"
     cd "$PKG_NAME"
 fi
 
-# Krok 4: Kontrola PKGBUILD souboru (Bezpečnostní doporučení Arch Linuxu)
 echo "==> Otevírám PKGBUILD ke kontrole. Pro ukončení prohlížení stiskněte 'q'..."
 sleep 5
 vim PKGBUILD
